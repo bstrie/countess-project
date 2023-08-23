b9173b1288f105bac2964f8a465cdc39943a76c9
bstrie <865233+bstrie@users.noreply.github.com>
2023-08-22 20:02:43 -0400

    Overload add

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@
 //   - mul
 //   - div
 //   - with self
-//   - with underlying type
 // - deref impl
 // - rest of the num interface
 // - generic
@@ -14,15 +13,20 @@
 // - macro'd
 // - intelligent checking strats
 
+// TODO: comment about deliberately not overloading ops on underlying num types
+
+use std::ops::{Add};
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub struct Foo {
-    val: u8,
+    val: i8,
 }
 
 // TODO: naming comment
 #[allow(non_snake_case)]
 // TODO: inline comment
 #[inline(always)]
-pub fn Foo(val: u8) -> Foo {
+pub fn Foo(val: i8) -> Foo {
     if val < Foo::MIN || val > Foo::MAX {
         panic!("Value out of bounds")
     } else {
@@ -31,19 +35,29 @@ pub fn Foo(val: u8) -> Foo {
 }
 
 impl Foo {
-    pub const MIN: u8 = 20;
-    pub const MAX: u8 = 80;
+    pub const MIN: i8 = -80;
+    pub const MAX: i8 = 80;
 
     // TODO: inline comment
     #[inline(always)]
     // TODO: use macros to make the body identical to Foo() for function merging opts
-    pub fn new(val: u8) -> Foo {
+    pub fn new(val: i8) -> Foo {
         Foo(val)
     }
 
     #[inline(always)]
-    pub fn value(&self) -> u8 {
+    pub fn value(&self) -> i8 {
         self.val
+    }
+}
+
+impl Add for Foo {
+    type Output = Self;
+
+    #[inline]
+    fn add(self, other: Self) -> Self {
+        let res = i8::checked_add(self.val, other.val).unwrap(); // panics on numeric overflow
+        Foo(res)
     }
 }
 
@@ -52,7 +66,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn constructors() {
+    fn construction() {
         let x = Foo(42);
         let y = Foo::new(42);
         assert_eq!(x.value(), y.value());
@@ -60,13 +74,51 @@ mod tests {
 
     #[test]
     fn bounds() {
-        assert_eq!(Foo::MIN, 20);
+        assert_eq!(Foo::MIN, -80);
         assert_eq!(Foo::MAX, 80);
     }
 
     #[test]
     #[should_panic]
-    fn out_of_bounds_construction() {
-        let _x = Foo(200);
+    fn min_oob_construction() {
+        let _x = Foo(-100);
+    }
+
+    #[test]
+    #[should_panic]
+    fn max_oob_construction() {
+        let _x = Foo(100);
+    }
+
+    #[test]
+    fn add() {
+        let x = Foo(-20);
+        let y = Foo(30);
+        let z = x + y;
+        assert_eq!(10, z.value());
+    }
+
+    #[test]
+    #[should_panic]
+    fn overflow_add() {
+        let _x = Foo(80) + Foo(80);
+    }
+
+    #[test]
+    #[should_panic]
+    fn underflow_add() {
+        let _x = Foo(-80) + Foo(-80);
+    }
+
+    #[test]
+    #[should_panic]
+    fn min_oob_add() {
+        let _x = Foo(-50) + Foo(-50);
+    }
+
+    #[test]
+    #[should_panic]
+    fn max_oob_add() {
+        let _x = Foo(50) + Foo(50);
     }
 }
